a7000bad4ecec3efcc1cb566cfaf82f588ccfe1b
himeshr <72493481+himeshr@users.noreply.github.com>
2021-11-29 14:04:30 +0530

    Update DivocSimulation to make use of V3/certify

--- perf/src/main/scala/perf/DIVOCSimulation.scala
+++ perf/src/main/scala/perf/DIVOCSimulation.scala
@@ -9,8 +9,8 @@ import scala.util.Random
 
 class DIVOCSimulation extends Simulation {
   private val env: String = System.getenv.getOrDefault("targetEnv", "dev")
-  var baseUrl = System.getenv.getOrDefault("baseUrl", "http://perf-divoc.xiv.in")
-  val token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJnUkVqVXA1blE3OG9RcEJjcjZzTFpTQlNyblV5U2dNMjBUaElteVE3dzlrIn0.eyJleHAiOjE2MjczMzIxODgsImlhdCI6MTYxMDA1MjIwMCwiYXV0aF90aW1lIjoxNjEwMDUyMTg4LCJqdGkiOiJiZTcwZTVlOS05MGFhLTQ2NGItOGFlZC1lMTM5YTViOTA3NDMiLCJpc3MiOiJodHRwOi8vZGl2b2MueGl2LmluL2F1dGgvcmVhbG1zL2Rpdm9jIiwiYXVkIjpbInJlYWxtLW1hbmFnZW1lbnQiLCJhY2NvdW50IiwiZmFjaWxpdHktYWRtaW4tcG9ydGFsIl0sInN1YiI6IjdiZDU4NTUxLTlhMmItNDllNC1iYWUzLWUwZjdlNmJmNWUyYyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImZhY2lsaXR5LWFwaSIsIm5vbmNlIjoiNTFiOTIwY2QtMjg1Zi00MzU3LWE3ODUtZDE3NzBiYmVlYWRmIiwic2Vzc2lvbl9zdGF0ZSI6IjZlYjFkYTliLTE3NzYtNDYxOS04ZGMxLWJiMzA3YTVmMzI0MCIsImFjciI6IjAiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cHM6Ly9kaXZvYy54aXYuaW4iLCJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCIqIiwiaHR0cHM6Ly9kaXZvYy1hcHAueGl2LmluIiwiaHR0cDovL2xvY2FsaG9zdDozMDAwIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsicmVhbG0tbWFuYWdlbWVudCI6eyJyb2xlcyI6WyJ2aWV3LXVzZXJzIiwicXVlcnktZ3JvdXBzIiwicXVlcnktdXNlcnMiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfSwiZmFjaWxpdHktYWRtaW4tcG9ydGFsIjp7InJvbGVzIjpbImZhY2lsaXR5LXN0YWZmIl19fSwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiMTExMTExMTE1MiIsImZhY2lsaXR5X2NvZGUiOiJNT0IyMzQxIn0.aDi3JS6tPMHs5yjZQntQucRs5eery7xRJJpJ9yymuQVQrC64gHHGCZ5sEdUxhgzzkUU0v8_jJzzsjSBNl7P1hqMm6V1xSrIhjNYHHOX3asueyKkTuqer2elo2iqMIiZoDRCrPQxOyI3ZDWTRqdGWUTt7Pc4CKfHdiCzez0A14T7c7WuwQiGzumUq34k640o4NJcZ5kMPRIfwAr3s53XfbnRxR7cT6GwvA2-a5Ak_dHCyjaQT5aUOsgCMdqPHHCfO-3Tv1U3cho6fdI7Dxdn3ZwlnC3jM5gbMhqqos0zcRrIT9JvEZfKxMXxGVqYnbrr9zsWTzjFb-UMEwfpnAenqDQ"
+  var baseUrl = System.getenv.getOrDefault("baseUrl", "http://localhost")
+  val token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJmaWwzSWNOZjlMRFV5WU9ZNlhQbmVjTC1QeURwUjBqajdqQk50QkpqRjBVIn0.eyJleHAiOjE2NTU0NDY5NTQsImlhdCI6MTYzODE2Njk1NSwianRpIjoiZWJhM2Q4OGUtYTc5Ni00ODNlLWEzZGUtMjRjZTBhMTIwYjIwIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdC9hdXRoL3JlYWxtcy9kaXZvYyIsImF1ZCI6WyJyZWFsbS1tYW5hZ2VtZW50IiwiYWNjb3VudCIsImZhY2lsaXR5LWFkbWluLXBvcnRhbCJdLCJzdWIiOiJkOWU2OGJlNC0yMDVhLTRiNDQtODMwMS0xZmVhMjU1N2YxY2YiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhZG1pbi1hcGkiLCJzZXNzaW9uX3N0YXRlIjoiOTAxOThlYmItMGQ4Ny00YTUzLWE2NTUtOTQ2NWI4MjdlOGYwIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwczovL2Rpdm9jLnhpdi5pbiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7InJlYWxtLW1hbmFnZW1lbnQiOnsicm9sZXMiOlsibWFuYWdlLXVzZXJzIl19LCJhZG1pbi1hcGkiOnsicm9sZXMiOlsiYXBpIl19LCJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX0sImZhY2lsaXR5LWFkbWluLXBvcnRhbCI6eyJyb2xlcyI6WyJmYWNpbGl0eS1zdGFmZiIsImFkbWluIiwibW9uaXRvcmluZyIsImZhY2lsaXR5LWFkbWluIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwiY2xpZW50SWQiOiJhZG1pbi1hcGkiLCJjbGllbnRIb3N0IjoiMTcyLjE4LjAuMTciLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsInByZWZlcnJlZF91c2VybmFtZSI6InNlcnZpY2UtYWNjb3VudC1hZG1pbi1hcGkiLCJjbGllbnRBZGRyZXNzIjoiMTcyLjE4LjAuMTcifQ.L7gCY94V2tiW44Mm9DTs54tIOuMYIxJn_sIgRHAKa0DgHiOYaSd0KpHSer-oX9k9WGkKlxsELkeEbbiLbF7tdMO8vZwZcUMne0b5x9ubQYjIfl5XCfIp7KhMM2rFD_ho2O6-ikDlW1Qohi4ls0O6Umysy3ZrmjkrkKeMiFtbg49jS6uUZ2fbtoS3gi-ColXWQFXlf3G377KyLbCmibGafeLDMk6Wo_XbuRDVskVNvJ9UWjYBhu4lQftPWEhaDYudSAmFNiq5up2v0nmXshAz3DF9THJ01dTeJylK59lJBSSRXUBLs3IOHO7LXiFDwmunlVTGQU9K2KnPQGsqZeoLbA"
   println(s"Using env ${baseUrl}")
 
   val httpProtocol = http
@@ -27,44 +27,67 @@ class DIVOCSimulation extends Simulation {
 
 //  val requestBody = RawFileBody("certificate-request.json")
 
-  val feeder = Iterator.continually(Map("id" -> Random.nextInt()))
+  val feeder = Iterator.continually(Map("id" -> Random.nextInt(Integer.MAX_VALUE)))
 
   val req = http("Create certificate")
-    .post(s"${baseUrl}/divoc/api/v1/certify")
+    .post(s"${baseUrl}/divoc/api/v3/certify")
     .headers(headers)
     .body(StringBody("""[
                        |  {
-                       |    "preEnrollmentCode": "12346${r}",
-                       |    "recipient": {
-                       |      "name": "Sneha K ${r}",
-                       |      "dob": "1957-01-30",
-                       |      "age":"63",
-                       |      "gender": "Female",
-                       |      "nationality": "Indian",
-                       |      "identity": "did:in.gov.uidai.aadhaar:111122223344",
-                       |      "contact": ["tel:${r}"]
-                       |    },
-                       |    "vaccination": {
-                       |      "name": "COVAXIN",
-                       |      "batch": "MB3428BX",
-                       |      "manufacturer": "Bharat Biotech",
-                       |      "date": "2020-12-02T19:21:19.646Z",
-                       |      "effectiveStart": "2020-12-15",
-                       |      "effectiveUntil": "2021-01-15"
-                       |    },
-                       |    "vaccinator": {
-                       |      "name": "Sooraj Singh"
-                       |    },
-                       |    "facility": {
-                       |      "name": "ABC Medical Center",
-                       |      "address": {
-                       |          "addressLine1" : "123, Koramangala",
-                       |          "addressLine2" :"",
-                       |          "district": "Bengaluru South",
-                       |          "state":"Karnataka",
-                       |          "pin" :560034
-                       |      }
-                       |    }
+                       |     "preEnrollmentCode": "12345${r}",
+                       |     "programId": "VCC001",
+                       |     "comorbidities": [
+                       |     
+                       |     ],
+                       |     "recipient": {
+                       |       "name": "John ${r}",
+                       |       "dob": "1987-04-21",
+                       |       "gender": "Male",
+                       |       "nationality": "Sri Lankan",
+                       |       "identity": "872550100V",
+                       |       "contact": [
+                       |       "tel:${r}"
+                       |     ],
+                       |     "address": {
+                       |       "addressLine1": "Tharaka, Kudawella South, Nakulugala, Tangalle",
+                       |       "addressLine2": "",
+                       |       "district": "Hambanthota",
+                       |       "state": "ceylon",
+                       |       "pincode": "123456"
+                       |     }
+                       |     },
+                       |     "vaccination": {
+                       |       "name": "Covishield",
+                       |       "batch": "41202025",
+                       |       "manufacturer": "india",
+                       |       "date": "2021-10-17T05:30:28.187Z",
+                       |       "effectiveStart": "2021-05-21",
+                       |       "effectiveUntil": "2022-05-21",
+                       |       "dose": 2,
+                       |       "totalDoses": 2
+                       |     },
+                       |     "vaccinator": {
+                       |       "name": "ss"
+                       |     },
+                       |     "facility": {
+                       |       "name": "MOH Gothatuwa",
+                       |       "address": {
+                       |       "addressLine1": "df",
+                       |       "addressLine2": "",
+                       |       "district": "wew",
+                       |       "state": "w",
+                       |       "pincode": "w"
+                       |     }
+                       |     },
+                       |     "meta": {
+                       |       "partialEnabled": true,
+                       |       "totalDoses": 2,
+                       |       "cit": "00000074",
+                       |       "name": "Covishield",
+                       |       "batch": "41202025",
+                       |       "manufacturer": "india",
+                       |       "date": "2021-07-17T05:30:28.187Z"
+                       |     }
                        |  }
                        |]""".stripMargin))
     .check(status.in(200))
@@ -72,16 +95,14 @@ class DIVOCSimulation extends Simulation {
 
   val scn = scenario("Certificate")
     .feed(feeder)
-    .forever() {
+    .forever(
       pace(1 seconds)
         .exec({s=>
-          s.set("r", Random.nextInt().toString)
+          s.set("r", Random.nextInt(Integer.MAX_VALUE).toString)
         }
         )
         .exec(req)
-
-    }
-
+    )
 
 //  setUp(scn.inject(atOnceUsers(1)))
 
